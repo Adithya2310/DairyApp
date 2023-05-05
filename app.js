d7c36269d66a74d42886b27b06c501fe0a4795d6
Adithya2310 <Adithyanandi099@gmail.com>
2023-05-05 10:45:28 +0530

    created a new home page with login and register button

--- app.js
+++ app.js
@@ -4,6 +4,25 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _=require('lodash');
 const mongoose=require('mongoose');
+const session=require('express-session');
+const passport=require('passport');
+const passportLocalMongoose=require('passport-local-mongoose');
+
+const app = express();
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+// for adding passport for authentication and cookies
+app.use(session({
+  secret:'secretKey',
+  resave:false,
+  saveUninitialized:false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.set('strictQuery',false);
 
@@ -20,27 +39,22 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const app = express();
-
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
 app.get("/",function(req,res){
-  Journal.find({},function(err,result)
-  {
-    if(err)
-    {
-      console.log(err);
-    }
-    else{
-      console.log(result);
-      res.render("home.ejs",{
-        homestartingcontent: homeStartingContent,
-        post: result
-      });
-    }
-  });
+  res.render("home.ejs");
+  // Journal.find({},function(err,result)
+  // {
+  //   if(err)
+  //   {
+  //     console.log(err);
+  //   }
+  //   else{
+  //     console.log(result);
+  //     res.render("home.ejs",{
+  //       homestartingcontent: homeStartingContent,
+  //       post: result
+  //     });
+  //   }
+  // });
 });
 
 app.get("/posts/:title",function(req,res){
@@ -86,7 +100,12 @@ app.get("/compose",function(req,res){
 
 app.get("/login",function(req,res){
   res.render("login.ejs");
-})
+});
+
+app.get("/register",function(req,res){
+  res.render("register.ejs");
+});
+
 app.post("/compose",function(req,res)
 {
   const journalEntry=new Journal({
